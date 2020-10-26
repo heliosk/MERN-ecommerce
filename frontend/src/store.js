1dd3d0fe8ad13e015409bf22b4c644ea7f004c79
Helio Kanashiro <helio.fototerra@gmail.com>
2020-10-26 11:52:15 -0300

    Register Screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,13 +6,14 @@ import {
   productDetailsReducer,
 } from './reducers/productReducers.js';
 import { cartReducer } from './reducers/cartReducers';
-import { userLoginReducer } from './reducers/usersReducers';
+import { userLoginReducer, userRegisterReducer } from './reducers/userReducers';
 
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   cart: cartReducer,
   userLogin: userLoginReducer,
+  userRegister: userRegisterReducer,
 });
 
 const cartItemsFromStorage = localStorage.getItem('cartItems')
