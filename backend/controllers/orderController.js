ac82c44cc3ea0e1b6b23795a50005457ac968c9c
Helio Kanashiro <helio.fototerra@gmail.com>
2020-10-30 13:10:05 -0300

    Endpoint para atualizar status para paid

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,4 +1,6 @@
 import asyncHandler from 'express-async-handler';
+import { Provider } from 'react-redux';
+import { orderDetailsReducer } from '../../frontend/src/reducers/orderReducers.js';
 import Order from '../models/orderModel.js';
 
 // @desc    Create new order
@@ -54,4 +56,29 @@ const getOrderById = asyncHandler(async (req, res) => {
   }
 });
 
-export { getOrderById, addOrderItems };
+// @desc    Update order to paid
+// @route   GET /api/orders/:id/pay
+// @access  Private
+const updateOrderToPaid = asyncHandler(async (req, res) => {
+  const order = await Order.findById(req.params.id);
+
+  if (order) {
+    order.isPaid = true;
+    order.paidAt = Date.now();
+    orderDetailsReducer.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+
+    const updatedOrder = await order.save();
+
+    res.json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+});
+
+export { getOrderById, addOrderItems, updateOrderToPaid };
